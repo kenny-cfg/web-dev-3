e17070fae93e1c54502b2c7caa75102269abc83d
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-07-25 19:26:46 +0100

    Serving HTML from root

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send("Poo bum");
+  res.sendFile(__dirname + '/frontend/index.html');
 })
 
 app.listen(3000, () => {
