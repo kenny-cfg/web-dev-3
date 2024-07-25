66b20ccbb32e02f4ac5ccccb8e886ab16645a15c
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-07-25 19:40:58 +0100

    Naive solution to serve the whole site

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/frontend/index.html');
 })
 
+app.get('/index.html', (req, res) => {
+  res.sendFile(__dirname + '/frontend/index.html');
+})
+
+app.get('/index.js', (req, res) => {
+  res.sendFile(__dirname + '/frontend/index.js');
+})
+
+app.get('/index.css', (req, res) => {
+  res.sendFile(__dirname + '/frontend/index.css');
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 })
